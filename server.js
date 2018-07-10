6dee1803947f69ba7f5a2471b633c46f5607567d
Yuhua <yuhua.wang@salesforce.com>
2018-07-10 14:40:57 -0700

    set up express and ejs

---
+++ server.js
@@ -0,0 +1,16 @@
+
+// boilerplate from Express documentation
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+// end boilerplate
+
+//set up template engine
+app.set('view engine', 'ejs')
